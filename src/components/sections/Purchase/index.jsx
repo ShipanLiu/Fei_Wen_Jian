745cb27f1970ab747b9dd66367208b018dab2d5d
Shipan <liushipan1998@gmail.com>
2021-06-22 09:25:44 +0200

    inline styled removed  Floating Background changed

--- src/components/sections/Purchase/index.jsx
+++ src/components/sections/Purchase/index.jsx
@@ -13,21 +13,11 @@ export default memo(function index() {
           <div className="d-flex flex-row justify-content-center flex-wrap">
             <div className="card m-3">
               <div className="card-body m-0 p-0">
-                <div
-                  className="title py-3 mb-2"
-                  style={{ background: 'rgba(140, 241, 241, 0.199)' }}
-                >
+                <div className="title py-3 mb-2">
                   <h5 className="card-title">FlyDocs</h5>
                 </div>
                 <p className="card-text pt-2">One Month</p>
-                <div
-                  className="pricing"
-                  style={{
-                    background: 'rgba(140, 241, 241, 0.199)',
-                    borderTopRightRadius: '55%',
-                    borderTopLeftRadius: '55%',
-                  }}
-                >
+                <div className="pricing">
                   <h1 className="my-5">$10</h1>
                   <a
                     href="#"
@@ -40,21 +30,11 @@ export default memo(function index() {
             </div>
             <div className="card m-3">
               <div className="card-body m-0 p-0">
-                <div
-                  className="title py-3 mb-2"
-                  style={{ background: 'rgba(140, 241, 241, 0.199)' }}
-                >
+                <div className="title py-3 mb-2">
                   <h5 className="card-title">FlyDocs</h5>
                 </div>
                 <p className="card-text pt-2">Three Month</p>
-                <div
-                  className="pricing"
-                  style={{
-                    background: 'rgba(140, 241, 241, 0.199)',
-                    borderTopRightRadius: '55%',
-                    borderTopLeftRadius: '55%',
-                  }}
-                >
+                <div className="pricing">
                   <h1 className="my-5">$28</h1>
                   <a
                     href="#"
@@ -67,21 +47,11 @@ export default memo(function index() {
             </div>
             <div className="card m-3">
               <div className="card-body m-0 p-0">
-                <div
-                  className="title py-3 mb-2"
-                  style={{ background: 'rgba(140, 241, 241, 0.199)' }}
-                >
+                <div className="title py-3 mb-2">
                   <h5 className="card-title">FlyDocs</h5>
                 </div>
                 <p className="card-text pt-2">One Year</p>
-                <div
-                  className="pricing"
-                  style={{
-                    background: 'rgba(140, 241, 241, 0.199)',
-                    borderTopRightRadius: '55%',
-                    borderTopLeftRadius: '55%',
-                  }}
-                >
+                <div className="pricing">
                   <h1 className="my-5">$110</h1>
                   <a
                     href="#"
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,13 @@
 import React, { memo } from 'react'
 import Welcome from '../../components/sections/Welcome/index'
-import Footer from '../../components/footer/index'
 import Purchase from '../../components/sections/Purchase/index'
+import FloatingBg from '../../components/floatingBg/FloatingBg'
 
 export default memo(function Home() {
   return (
     <div>
       <main>
+        <FloatingBg></FloatingBg>
         <Welcome></Welcome>
         <Purchase></Purchase>
       </main>
