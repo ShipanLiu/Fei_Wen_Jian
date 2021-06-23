d50bc80f3616b3a20ee8aea7189c7d61712c30ef
Shipan <liushipan1998@gmail.com>
2021-06-23 09:53:35 +0200

    simgle img upload

--- src/components/upload/index.jsx
+++ src/components/upload/index.jsx
@@ -2,13 +2,45 @@ import React, { memo, useRef, useCallback, useState, useEffect } from 'react'
 import './style.scss'
 
 export default memo(function Upload() {
+  const [image, setImage] = useState(null)
+  const fileInputRef = useRef()
+  // once img changed, rerender and show the extracted pic
+  useEffect(() => {
+    if (image) {
+      console.log('进入useEffect')
+      // console.log(image)
+    } else {
+      console.log('image do not exists')
+    }
+  }, [image])
+
   const handleClick = useCallback((e) => {
     console.log(e.target)
   })
 
-  const [img, setImg] = useState()
+  const handleOnChange = (e) => {
+    if (e.target.files) {
+      console.log('success')
+      const file = e.target.files[0]
+      const reader = new FileReader()
+      reader.onload = function (e) {
+        const img = new Image()
+        img.src = reader.result
+        // console.log(img.src)
+        setImage(img.src)
+      }
+      reader.readAsDataURL(file)
+    } else {
+      setImage(null)
+      console.log('e.target.files failure')
+    }
+  }
 
-  const fileInputRef = useRef()
+  const handleDelete = () => {
+    console.log('delete')
+    setImage(null)
+  }
+
   return (
     <div className="upload-wrapper">
       <div className="container">
@@ -17,28 +49,31 @@ export default memo(function Upload() {
         </div>
         <hr />
         <div className="upload-part row">
-          <button
-            className="btn btn-info row-item col-6 col-md-4 col-lg-3 mx-2 border-0"
-            onClick={(e) => {
-              handleClick(e)
-              fileInputRef.current.click()
-            }}
-          >
-            <i className="bi bi-arrow-up text-light"></i>
-          </button>
+          {image ? (
+            <div className="showed-img row-item col-6 col-md-4 col-lg-3 mx-2 border-0">
+              <i
+                className="delete-icon bi bi-x bg-danger"
+                onClick={handleDelete}
+              ></i>
+              <img src={image} alt="photo parse wrong" className="img-fluid" />
+            </div>
+          ) : (
+            <button
+              className="btn btn-info row-item col-6 col-md-4 col-lg-3 mx-2 border-0"
+              onClick={(e) => {
+                handleClick(e)
+                fileInputRef.current.click()
+              }}
+            >
+              <i className="bi bi-arrow-up text-light"></i>
+            </button>
+          )}
           <input
             type="file"
             accept="image/*"
             ref={fileInputRef}
             className="upload-input col-6 col-md-4 col-lg-3 mx-2"
-            onChange={(e) => {
-              const currentFile = e.target.files[0]
-              if (currentFile) {
-                setImg(currentFile)
-              } else {
-                setImg(null)
-              }
-            }}
+            onChange={handleOnChange}
           />
         </div>
       </div>
