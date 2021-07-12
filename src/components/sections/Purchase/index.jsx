39499b85a74756cafd961b122f768f4ff981bec9
Shipan <liushipan1998@gmail.com>
2021-07-12 09:52:22 +0200

    Animation added

--- src/components/sections/Purchase/index.jsx
+++ src/components/sections/Purchase/index.jsx
@@ -1,18 +1,24 @@
-import React, { memo } from 'react'
+import React from 'react'
 import PurchaseCard from '../purchase-card/PurchaseCard'
+import { useScroll } from '../../../hooks/useScroll'
+import { motion } from 'framer-motion'
+import { textAnimation, cardAnimation } from '../../../utils/Animation'
 import './style.scss'
 
-export default memo(function index() {
+export default function Purchase() {
+  const [element, controls] = useScroll()
   const priceCard = [
     { id: 1, duration: 'One Month', price: '$10' },
     { id: 2, duration: 'three Month', price: '$28' },
     { id: 3, duration: 'One Year', price: '$110' },
   ]
   return (
-    <div className="my-5">
+    <div className="my-5" ref={element}>
       <div className=" purchase text-center">
-        <h1 className="text-white">Purchase Whatever You Want</h1>
-        <div className="cards m-3">
+        <div>
+          <h1 className="text-white">Purchase Whatever You Want</h1>
+        </div>
+        <motion.div className="cards m-3">
           <div className="d-flex flex-row justify-content-center flex-wrap">
             {priceCard.map((item) => {
               return (
@@ -20,12 +26,14 @@ export default memo(function index() {
                   key={item.id}
                   duration={item.duration}
                   price={item.price}
+                  variants={cardAnimation}
+                  animate={controls}
                 ></PurchaseCard>
               )
             })}
           </div>
-        </div>
+        </motion.div>
       </div>
     </div>
   )
-})
+}
--- src/components/sections/Welcome/index.jsx
+++ src/components/sections/Welcome/index.jsx
@@ -1,23 +1,39 @@
 import React from 'react'
+import { useScroll } from '../../../hooks/useScroll'
+import { motion } from 'framer-motion'
+import WorkImage from '../../../assets/img/work.svg'
 import './style.scss'
+import { headerAnimation, imageAnimation } from '../../../utils/Animation'
 
-const home = () => {
+const Welcome = () => {
+  const [element, controls] = useScroll()
   return (
-    <section id="home" className="container">
-      <div>
-        <div className="home-content p-5">
-          <div className="intro text-center text-light">
-            <h1 className="mb-3">WELCOME</h1>
-            <h2 className="sub-title mb-4"> use our fly docs services</h2>
-            <button className="btn btn-primary border-0 mx-3">
-              Learn More
-            </button>
-            <button className="btn btn-info border-0 mx-2">Contact Us</button>
+    <div id="welcome" className="container" ref={element}>
+      <div className="row">
+        <motion.div
+          className="content col-12 col-md-5"
+          variants={headerAnimation}
+          animate={controls}
+          transition={{ delay: 0.2, type: 'tween' }}
+        >
+          <h1 className="text-white">Welcome</h1>
+          <p className="text-white">A professional web for Docs sharing</p>
+          <div className="button-container d-flex flex-nowrap justify-content-center align-items-center my-5">
+            <button className="btn btn-primary mx-2 col-6">watch video</button>
+            <button className="btn btn-primary col-6">know more</button>
           </div>
-        </div>
+        </motion.div>
+        <motion.div
+          className=" col-12 col-md-7"
+          variants={imageAnimation}
+          animate={controls}
+          transition={{ type: 'tween' }}
+        >
+          <img src={WorkImage} alt="Work" className="img-fluid" />
+        </motion.div>
       </div>
-    </section>
+    </div>
   )
 }
 
-export default home
+export default Welcome
